28b017c3f69fb5dc7f0b78b5c076f998cb3d336e
KrisPersson <112572136+KrisPersson@users.noreply.github.com>
2024-05-28 11:56:32 +0200

    alt for image

--- src/styled-components/components/ProfilePicBox/index.tsx
+++ src/styled-components/components/ProfilePicBox/index.tsx
@@ -31,7 +31,7 @@ type ProfilePicBoxProps = {
 export default function ProfilePicBox({ imgSrc }: ProfilePicBoxProps) {
   return (
     <Wrapper>
-      <StyledImg src={imgSrc} fill />
+      <StyledImg src={imgSrc} fill alt="Profile picture of Kristofer" />
     </Wrapper>
   );
 }
