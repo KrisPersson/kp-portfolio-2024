17f08207fc0dd9beea88f511e210858fbd914e4b
KrisPersson <112572136+KrisPersson@users.noreply.github.com>
2024-05-28 11:52:32 +0200

    paragraph temp literal

--- src/styled-components/components/Paragraph/index.tsx
+++ src/styled-components/components/Paragraph/index.tsx
@@ -1,7 +1,7 @@
 import { styled } from "styled-components";
 
 export const Subheading = styled.p<{ $color?: string }>`
-  color: ${(props) => props.theme.colors[props.$color] || "currentColor"};
+  color: ${(props) => props.theme.colors[`${props.$color}`] || "currentColor"};
   font-weight: ${(props) => props.theme.font.weight.medium};
   font-size: ${(props) => props.theme.font.size.large};
   letter-spacing: 1.2px;
@@ -10,7 +10,7 @@ export const Subheading = styled.p<{ $color?: string }>`
 `;
 
 export const Paragraph = styled.p<{ $color?: string }>`
-  color: ${(props) => props.theme.colors[props.$color] || "currentColor"};
+  color: ${(props) => props.theme.colors[`${props.$color}`] || "currentColor"};
   font-weight: ${(props) => props.theme.font.weight.regular};
   font-size: ${(props) => props.theme.font.size.regular};
   letter-spacing: 0.8px;
